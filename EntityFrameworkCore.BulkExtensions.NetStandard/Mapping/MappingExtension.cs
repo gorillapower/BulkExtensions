feb757ab70bf2188b2e56fbd79cd9e275aec205e
Martin Power <martinispower@gmail.com>
2021-03-18 20:19:07 +0200

    upgraded to netcore5

--- EntityFrameworkCore.BulkExtensions.NetStandard/Mapping/MappingExtension.cs
+++ EntityFrameworkCore.BulkExtensions.NetStandard/Mapping/MappingExtension.cs
@@ -21,7 +21,7 @@ namespace EntityFramework.BulkExtensions.Mapping
             if (entityType == null)
                 throw new BulkException(@"Entity is not being mapped by Entity Framework. Verify your EF configuration.");
 
-            var relational = entityType.Relational();
+            //var relational = entityType.GetDiscriminatorProperty().n;
             var baseType = entityType.GetSetType();
             var hierarchy = context.Model.GetEntityTypes()
                 .Where(type => type.BaseType == null ? type == baseType : type.GetSetType() == baseType)
@@ -30,17 +30,17 @@ namespace EntityFramework.BulkExtensions.Mapping
 
             var entityMapping = new EntityMapping
             {
-                TableName = relational.TableName,
-                Schema = relational.Schema
+                TableName = entityType.GetTableName(),
+                Schema = entityType.GetSchema()
             };
 
             if (hierarchy.Count > 1 &&
-                !properties.Any(property => property.ColumnName.Equals(relational.DiscriminatorProperty.Name)))
+                !properties.Any(property => property.ColumnName.Equals(entityType.GetDiscriminatorProperty().Name)))
             {
                 entityMapping.HierarchyMapping = GetHierarchyMappings(hierarchy);
                 properties.Add(new PropertyMapping
                 {
-                    ColumnName = relational.DiscriminatorProperty.Name,
+                    ColumnName = entityType.GetDiscriminatorProperty().Name,
                     IsHierarchyMapping = true
                 });
             }
@@ -57,19 +57,19 @@ namespace EntityFramework.BulkExtensions.Mapping
         private static Dictionary<string, string> GetHierarchyMappings(IEnumerable<IEntityType> hierarchy)
         {
             return hierarchy.ToDictionary(entityType => entityType.ClrType.Name,
-                entityType => entityType.Relational().DiscriminatorValue.ToString());
+                entityType => entityType.GetDiscriminatorValue().ToString());
         }
 
         private static IEnumerable<IPropertyMapping> GetPropertyMappings(this IEnumerable<IEntityType> hierarchy)
         {
             return hierarchy
-                .SelectMany(type => type.GetProperties().Where(property => !property.IsShadowProperty))
+                .SelectMany(type => type.GetProperties().Where(property => !property.IsShadowProperty()))
                 .Distinct()
                 .ToList()
                 .Select(property => new PropertyMapping
                 {
                     PropertyName = property.Name,
-                    ColumnName = property.Relational().ColumnName,
+                    ColumnName = property.GetColumnName(),
                     IsPk = property.IsPrimaryKey(),
                     IsFk = property.IsForeignKey(),
                     IsDbGenerated = property.ValueGenerated != ValueGenerated.Never
